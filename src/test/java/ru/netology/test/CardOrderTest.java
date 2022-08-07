a4381a50a4f0fed43fbd47d438bde235b383cb53
Dmitri Romanov <investdjr@gmail.com>
2022-08-07 22:51:50 +0300

    commited

---
+++ src/test/java/ru/netology/test/CardOrderTest.java
@@ -0,0 +1,20 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+
+    @Test
+    void submittingAForm() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Романов Дмитрий");
+        $("[data-test-id=phone] input").setValue("+78910000000");
+        $("[data-test-id=agreement]").click();
+        $("button[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
